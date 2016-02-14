b5ebc3a818afcb855af95de8e64a1ec677e16bc3
Darklg <kev.rocher@gmail.com>
2016-02-14 23:25:20 +0100

    v 0.2
    * Add crontab.
    * Add more hooks.
    * Import images.

--- README.md
+++ README.md
@@ -13,12 +13,24 @@ function basic_wpursstoposts_feeds($feeds) {
 }
 ```
 
+Hooks
+---
+
+* wpursstoposts_maxitems : (int) Numbers of items parsed (default: 15).
+* wpursstoposts_importimg : (bool) Import post images or not. (default: true).
+* wpursstoposts_posttype : (string) Post type id used to store imported items (default: rss).
+* wpursstoposts_posttype_info : (array) Args for default post type.
+* wpursstoposts_feeds : (array) URLs of feeds to parse (default: empty).
+
 
 Todo
 ---
 
-* [ ] Add sources with an admin page.
-* [ ] Set crontab to avoid background checks.
-* [ ] Import images & enclosures.
-* [ ] More hooks.
+* [*] Set crontab to avoid background checks.
+* [*] Import images.
+* [*] More hooks.
+* [ ] Create taxonomy from feed url.
+* [ ] Add sources from an admin page.
+* [ ] Settings from an admin page.
+* [ ] Create post thumbnail with first image.
 
--- wpursstoposts.php
+++ wpursstoposts.php
@@ -3,7 +3,7 @@
 /*
 Plugin Name: WPU RSS to posts
 Plugin URI: https://github.com/WordPressUtilities/wpursstoposts
-Version: 0.1
+Version: 0.2
 Description: Easily import RSS into posts
 Author: Darklg
 Author URI: http://darklg.me/
@@ -14,38 +14,55 @@ License URI: http://opensource.org/licenses/MIT
 class wpursstoposts {
 
     private $maxitems = 15;
-    private $post_type = 'rss';
+    private $posttype = 'rss';
+    private $importimg = true;
+    private $hookcron = 'wpursstoposts_crontab';
 
     public function __construct() {
         add_action('init', array(&$this, 'init'));
+        $this->maxitems = apply_filters('wpursstoposts_maxitems', $this->maxitems);
+        $this->posttype = apply_filters('wpursstoposts_posttype', $this->posttype);
+        $this->importimg = apply_filters('wpursstoposts_importimg', $this->importimg);
     }
 
     public function init() {
-        register_post_type($this->post_type,
-            array(
-                'labels' => array(
-                    'name' => __('RSS items'),
-                    'singular_name' => __('RSS item')
-                ),
-                'public' => true,
-                'has_archive' => true
-            )
-        );
 
-        // Parse every hour.
-        if (false === get_transient('wpursstoposts_import_feeds')) {
-            set_transient('wpursstoposts_import_feeds', 1, 1 * HOUR_IN_SECONDS);
-            $this->parse_feeds();
+        // RSS Post type
+        $posttype_info = apply_filters('wpursstoposts_posttype_info', array(
+            'labels' => array(
+                'name' => __('RSS items'),
+                'singular_name' => __('RSS item')
+            ),
+            'public' => true,
+            'has_archive' => true
+        ));
+
+        register_post_type($this->posttype, $posttype_info);
+
+        // Launch cron every
+        if (!wp_next_scheduled($this->hookcron)) {
+            wp_schedule_event(time(), 'hourly', $this->hookcron);
         }
 
+        add_action($this->hookcron, array(&$this, 'crontab_action'));
+    }
+
+    public function crontab_action() {
+        $this->parse_feeds();
     }
 
     /* Import
     -------------------------- */
 
     public function parse_feeds() {
+
         global $wpdb;
         include_once ABSPATH . WPINC . '/feed.php';
+        if ($this->importimg) {
+            require_once ABSPATH . 'wp-admin/includes/media.php';
+            require_once ABSPATH . 'wp-admin/includes/file.php';
+            require_once ABSPATH . 'wp-admin/includes/image.php';
+        }
 
         // Extract RSS feeds
         $feeds = apply_filters('wpursstoposts_feeds', array());
@@ -84,7 +101,7 @@ class wpursstoposts {
             'post_title' => wp_strip_all_tags($item->get_title()),
             'post_content' => $item->get_content(),
             'post_status' => 'publish',
-            'post_type' => $this->post_type,
+            'post_type' => $this->posttype,
             'post_date' => $item->get_date('Y-m-d H:i:s')
         ));
 
@@ -92,10 +109,42 @@ class wpursstoposts {
             return false;
         }
 
+        // Extract images
+        if ($this->importimg) {
+            $this->import_images_from_content($post_id, $item->get_content());
+        }
+
         // Save permalink
         update_post_meta($post_id, 'rss_permalink', $item->get_permalink());
 
         return $post_id;
+    }
+
+    public function import_images_from_content($post_id, $post_content) {
+        $regex_image = '/http:\/\/[^?#]+\.(?:jpe?g|png|gif)/Ui';
+        preg_match_all($regex_image, $post_content, $matches);
+
+        if (!isset($matches[0]) || empty($matches[0])) {
+            return false;
+        }
+
+        // For each image found
+        foreach ($matches[0] as $img_url) {
+            // Import image
+            $image = media_sideload_image($img_url, $post_id, '');
+            if (!is_wp_error($image)) {
+                preg_match($regex_image, $image, $new_image_url);
+                if (isset($new_image_url[0]) && !empty($new_image_url[0])) {
+                    $post_content = str_replace($img_url, $new_image_url[0], $post_content);
+                }
+            }
+        }
+
+        // update content
+        wp_update_post(array(
+            'ID' => $post_id,
+            'post_content' => $post_content
+        ));
     }
 
     /* Values
